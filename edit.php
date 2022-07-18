728e83ef0145ea5e704208374e5027293058aff2
Michael <michaeldrpabaya@gmail.com>
2022-07-18 19:32:57 +0800

    Fixed indention in register/edit.php

--- edit.php
+++ edit.php
@@ -10,44 +10,47 @@ $row = $students->fetch_assoc();
 
 if (isset($_POST['submit'])) {
 
- $fname = $_POST['firstname'];
- $lname = $_POST['lastname'];
- $gender = $_POST['gender'];
- $sql = "UPDATE farmer_list SET first_name = '$fname', last_name = '$lname', gender = '$gender' WHERE id = '$id'";
- -$con->query($sql) or die($con->error);
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $gender = $_POST['gender'];
+    $sql = "UPDATE farmer_list SET first_name = '$fname', last_name = '$lname', gender = '$gender' WHERE id = '$id'";
+    -$con->query($sql) or die($con->error);
 
- echo header("Location: details.php?ID=" . $id);
+    echo header("Location: details.php?ID=" . $id);
 }
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Student Management System</title>
- <link rel="stylesheet" href="./css/style.css">
- <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
 </head>
 
 <body>
- <form action="" method="post">
-  <label>First Name</label>
-  <input type="text" name="firstname" id="firstname" value="<?php echo $row['first_name']; ?>">
+<form action="" method="post">
+    <label>First Name</label>
+    <input type="text" name="firstname" id="firstname" value="<?php echo $row['first_name']; ?>">
 
-  <label>Last Name</label>
-  <input type="text" name="lastname" id="lastname" value="<?php echo $row['last_name']; ?>">
+    <label>Last Name</label>
+    <input type="text" name="lastname" id="lastname" value="<?php echo $row['last_name']; ?>">
 
-  <label>Gender</label>
-  <select name="gender" id="gender">
-   <option value="Male" <?php echo ($row['gender'] == "Male") ? 'selected' : ''; ?>>Male</option>
-   <option value="Female" <?php echo ($row['gender'] == "Female") ? 'selected' : ''; ?>>Female</option>
-  </select>
+    <label>Gender</label>
 
-  <input type="submit" name="submit" value="Submit Form">
- </form>
+    <select name="gender" id="gender">
+    <option value="Male" <?php echo ($row['gender'] == "Male") ? 'selected' : ''; ?>>Male</option>
+    <option value="Female" <?php echo ($row['gender'] == "Female") ? 'selected' : ''; ?>>Female</option>
+    </select>
+
+    <input type="submit" name="submit" value="Submit Form">
+    
+</form>
 </body>
 
 </html>
