615baa52a0723e799a60eae5a8c201f895de69e5
Michael <michaeldrpabaya@gmail.com>
2022-07-19 13:18:20 +0800

    fixed display and removed the animation on button and logo

--- landing.php
+++ landing.php
@@ -33,91 +33,97 @@ $row = $students->fetch_assoc();
   <link rel="stylesheet" href="./css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
+  <!-- fontawesome cdn -->
+  <script src="https://use.fontawesome.com/370fcb8630.js"></script>
 </head>
 
 <body>
+<div class="page min-vh-100">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg py-2 px-5" id="navigation-bar">
 
-<nav class="text-center navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./about.html">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./products.html">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./contact.html">Contact</a>
-        </li>
-      </ul>
+        <div class="collapse navbar-collapse d-flex justify-content-around" id="navbarSupportedContent">
 
-      <a class="navbar-brand" href="#">
-        <img src="./img/grow_soils.webp" alt="logo" height="35">
-      </a>
+          <a class="navbar-brand" href="#">
+              <img src="./img/grow_soils.webp" alt="logo" height="35">
+          </a>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-          
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <?php if (isset($_SESSION['UserLogin'])) { ?>
-          <a class="nav-link" href="index.php">Farmer List</a>
-          <?php  } else { ?>
-          <a class="nav-link" href="register.html">Register</a>
-          <?php } ?>
-        </li>
-        <li class="nav-item">
-          <?php if (isset($_SESSION['UserLogin'])) { ?>
-          <a class="nav-link" href="logout.php">Logout</a>
-          <?php  } else { ?>
-          <a class="nav-link" href="login.php">Login</a>
-          <?php } ?>
-        </li>
-      </ul>
+          <ul class="navbar-nav">
+              <li class="nav-item px-2">
+                  <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
+              </li>
+              <li class="nav-item px-2">
+                  <a class="nav-link" href="./about.html">About</a>
+              </li>
+              <li class="nav-item px-2">
+                  <a class="nav-link" href="./products.html">Services</a>
+              </li>
+              <li class="nav-item px-2">
+                  <a class="nav-link" href="./contact.html">Contact</a>
+              </li>
+              <li class="nav-item px-2">
+                  <?php if (isset($_SESSION['UserLogin'])) { ?>
+                  <a class="nav-link" href="index.php">Farmer List</a>
+                  <?php  } else { ?>
+                  <a class="nav-link" href="register.html">Register</a>
+                  <?php } ?>
+              </li>
+              <li class="nav-item px-2">
+                  <?php if (isset($_SESSION['UserLogin'])) { ?>
+                  <a class="nav-link" href="logout.php">Logout</a>
+                  <?php  } else { ?>
+                  <a class="nav-link" href="login.php">Login</a>
+                  <?php } ?>
+              </li>
+          </ul>
 
-    </div>
-  </div>
-</nav>
+          <!--SearchBar-->
+          <form>
+            <input class="form-control px-1 py-1 rounded-0" type="search" placeholder="Search" aria-label="Search">
+            <!-- idk why this icon is not working -->
+            <i class="fa-solid fa-magnifying-glass-location"></i>
+          </form>
 
-<div class="masthead" style="background-image: url('./img/background.webp');">
-<div class="color-overlay d-flex justify-content-center align-items-center">
-<!--showcase flexbox method text-sm-start aligns text to left-->
+          <button 
+              class="navbar-toggler" 
+              type="button" 
+              data-bs-toggle="collapse" 
+              data-bs-target="#navbarSupportedContent" 
+              aria-controls="navbarSupportedContent" 
+              aria-expanded="false" 
+              aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
 
-<section class="text-light p-sm-5 pb-lg-5 pt-lg-5 text-center text-sm-start">
-  <br>
-    <div class="container">
-      <div class="d-sm-flex align-items-center justify-content-between">
-        <div class="col-sm">
+        </div>
 
-          <div class="hero-text px-2 py-1 mt-sm-5 rounded">
+    </nav>
 
-          <h2><span class="samotsari-color hero-title">Grow Soils Philippines</span></h2>
+    <section class="d-flex justify-content-center text-center">
 
-          <p class="lead my-2">Sinusuportahan natin ang ating mahal na mga <b class="text-warning">Magsasaka</b> upang matulungan sila sa kalidad ng kanilang mga ani.
-          </p>
+      <div class="row">
 
-          <p class="lead">In support and in cooperation with the <b>Department of Agriculture</b>, Grow Soils Ph plans to provide up to date soil data and analysis for all farmers.
-          </p>
+          <div class="col-12">
+              <img class="img-fluid hero-img w-50 p-4" src="./img/grow_soils.webp" alt="">
+          </div>
+
+          <div class="col-12">
+
+              <h2><span class="samotsari-color hero-title">Grow Soils Philippines</span></h2>
+
+              <p class="lead my-2">Sinusuportahan natin ang ating mahal na mga <b class="text-warning">Magsasaka</b> upang matulungan sila sa kalidad ng kanilang mga ani.
+              </p>
+
+              <p class="lead">In support and in cooperation with the <b>Department of Agriculture</b>, Grow Soils Ph plans to provide up to date soil data and analysis for all farmers.
+              </p>
+
+              <a href="./register.html"><button class="btn btn-success">Iparehistro Ang Iyong Lupa</button></a>
 
           </div>
-          
-          <a href="./register.html"><button class="mt-2 hero-btn btn btn-primary btn-lg">Iparehistro Ang Iyong Lupa</button></a>
 
       </div>
-      <!-- d-none makes image invisible at mobile devices-->
-      <!-- d-sm-block makes image visible at breakpoint-->
-      <img class="img-fluid hero-img d-md-block d-none w-50 p-4" src="./img/grow_soils.webp" alt="">
-    </div>
-  </div>
-</section>
 
-</div>
+    </section>
 </div>
 
 <footer class="footer"><small>&copy; <a href="./index.php">Grow Soils Ph</a></small></footer>
