fb3dada531185f72ae04722fce86e4285714ad1c
Michael <michaeldrpabaya@gmail.com>
2022-07-19 17:05:26 +0800

    created landing page but Lorem

--- landing.php
+++ landing.php
@@ -39,8 +39,11 @@ $row = $students->fetch_assoc();
 
 <body>
 <div class="page min-vh-100">
+    
+    <div class="masthead" style="background-image: url('./img/noah-buscher-x8ZStukS2PM-unsplash.jpg'); height: 100vh">
+    
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg py-2 px-5" id="navigation-bar">
+    <nav class="navbar navbar-expand-lg py-1 px-5" id="navigation-bar">
 
         <div class="collapse navbar-collapse d-flex justify-content-around" id="navbarSupportedContent">
 
@@ -48,40 +51,37 @@ $row = $students->fetch_assoc();
               <img src="./img/grow_soils.webp" alt="logo" height="35">
           </a>
 
-          <ul class="navbar-nav">
+          <ul class="navbar-nav ms-5">
               <li class="nav-item px-2">
-                  <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
+                  <a class="nav-link active  text-light" aria-current="page" href="./index.html">Home</a>
               </li>
               <li class="nav-item px-2">
-                  <a class="nav-link" href="./about.html">About</a>
+                  <a class="nav-link  text-light" href="./about.html">About</a>
               </li>
               <li class="nav-item px-2">
-                  <a class="nav-link" href="./products.html">Services</a>
+                  <a class="nav-link  text-light" href="./products.html">Services</a>
               </li>
               <li class="nav-item px-2">
-                  <a class="nav-link" href="./contact.html">Contact</a>
+                  <a class="nav-link  text-light" href="./contact.html">Contact</a>
               </li>
               <li class="nav-item px-2">
                   <?php if (isset($_SESSION['UserLogin'])) { ?>
-                  <a class="nav-link" href="index.php">Farmer List</a>
+                  <a class="nav-link  text-light" href="index.php">Farmer List</a>
                   <?php  } else { ?>
-                  <a class="nav-link" href="register.html">Register</a>
+                  <a class="nav-link  text-light" href="register.html">Register</a>
                   <?php } ?>
               </li>
               <li class="nav-item px-2">
                   <?php if (isset($_SESSION['UserLogin'])) { ?>
-                  <a class="nav-link" href="logout.php">Logout</a>
+                  <a class="nav-link  text-light" href="logout.php">Logout</a>
                   <?php  } else { ?>
-                  <a class="nav-link" href="login.php">Login</a>
+                  <a class="nav-link  text-light" href="login.php">Login</a>
                   <?php } ?>
               </li>
           </ul>
 
-          <!--SearchBar-->
           <form>
             <input class="form-control px-1 py-1 rounded-0" type="search" placeholder="Search" aria-label="Search">
-            <!-- idk why this icon is not working -->
-            <i class="fa-solid fa-magnifying-glass-location"></i>
           </form>
 
           <button 
@@ -99,35 +99,61 @@ $row = $students->fetch_assoc();
 
     </nav>
 
-    <section class="d-flex justify-content-center text-center">
+    <section class="d-flex justify-content-center align-items-center text-center">
 
       <div class="row">
 
           <div class="col-12">
-              <img class="img-fluid hero-img w-50 p-4" src="./img/grow_soils.webp" alt="">
+              <img class="hero-img w-50 p-4" src="./img/grow_soils.webp" alt="Grow Soils">
           </div>
 
-          <div class="col-12">
+          <div class="col-12 para py-3">
 
-              <h2><span class="samotsari-color hero-title">Grow Soils Philippines</span></h2>
+              <h3 class="lead my-2" id="para">Sinusuportahan natin ang ating mahal na mga <span class="important"><b>Magsasaka</b></span> upang matulungan sila sa kalidad ng kanilang mga ani.
+              </h3>
 
-              <p class="lead my-2">Sinusuportahan natin ang ating mahal na mga <b class="text-warning">Magsasaka</b> upang matulungan sila sa kalidad ng kanilang mga ani.
-              </p>
+              <h3 class="lead" id="para">In support and in cooperation with the <span class="important"><b>Department of Agriculture</b></span>, Grow Soils Ph plans to provide up to date soil data and analysis for all farmers.
+              </h3>
 
-              <p class="lead">In support and in cooperation with the <b>Department of Agriculture</b>, Grow Soils Ph plans to provide up to date soil data and analysis for all farmers.
-              </p>
+          </div>
 
-              <a href="./register.html"><button class="btn btn-success">Iparehistro Ang Iyong Lupa</button></a>
-
+          <div class="col-12 mt-3">
+          <a href="./register.html"><button class="rounded-3 text-light">Iparehistro Ang Iyong Lupa</button></a>
           </div>
 
       </div>
 
     </section>
+
 </div>
 
-<footer class="footer"><small>&copy; <a href="./index.php">Grow Soils Ph</a></small></footer>
+<!-- i removed this temporarily but i had to ask this to sir ronaldo for confirmation -->
+<!-- <footer><small>&copy; <a href="./index.php">Grow Soils Ph</a></small></footer> -->
+<footer>
+  <div class="container">
+    <div class="row">
+    
+      <div class="col-12 text-center border-bottom border-1 d-flex justify-content-around">
+        <h1>Logo</h1>
+        <a href="#"><h2">Lorem</h2></a>
+        <a href="#"><h2">Lorem</h2></a>
+        <a href="#"><h2">Lorem</h2></a>
+        <a href="#"><h2">Lorem</h2></a>
+        <a href="#"><h2">Lorem</h2></a>
 
+        <a href="#"><i class="fa-brands fa-facebook-square">fb</i></a>
+        <a href="#"><i class="fa-brands fa-facebook-square">fb</i></a>
+        <a href="#"><i class="fa-brands fa-facebook-square">fb</i></a>
+
+      </div>
+
+      <div class="col-12 footer-info text-center pt-3">
+        <a href="./index.php">Grow Soils Ph</a>
+      </div>
+
+    </div>
+  </div>
+</footer>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </html>
